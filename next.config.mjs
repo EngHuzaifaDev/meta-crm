395889778d9194d4a479697eea520aa4ea07ca83
EngHuzaifaDev <eng.huzaifa.dev@gmail.com>
2026-07-29 00:37:14 +0500

    issue fix +2

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,11 @@ const nextConfig = {
   },
   experimental: {
     serverActions: {
-      allowedOrigins: [process.env.NEXT_PUBLIC_APP_URL].filter(Boolean),
+      allowedOrigins: [
+        process.env.NEXT_PUBLIC_APP_URL,
+        "http://localhost:3003",
+        "https://metacrm.sarrast.cloud",
+      ].filter(Boolean),
     },
   },
 };
